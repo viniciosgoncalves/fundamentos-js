e7b8d2c9a2c8b10644803e454513af6b623db473
Vinicios Jose Gonçalves de Jesus <vinicios.goncalves@compasso.com.br>
2023-10-26 10:02:31 -0300

    exemplos da aula 2 conversoes

---
+++ conversoes.js
@@ -0,0 +1,22 @@
+// TIPO DE DADO
+// BOOLEANOS
+
+//conversão implícita
+//const numero = 455;
+//const numeroString = "456";
+
+//console.log (numero === numeroString)
+
+//conversão explicita
+//const numero = 456;
+//const numeroString = "456";
+
+//console.log (numero == numeroString)
+
+const numero = 456;
+const numeroString = "456";
+
+// converter o valor que numeroString que esta como string e não number
+//Number ()
+//String ()
+console.log (numero + Number (numeroString))
