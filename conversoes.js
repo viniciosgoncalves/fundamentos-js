8f8c175cc2b83ae7fd08cda1ca5c45d1e2b8288f
Vinicios Jose Gonçalves de Jesus <vinicios.goncalves@compasso.com.br>
2023-10-26 10:10:21 -0300

    exemplos da aula 2 conversoes

--- conversoes.js
+++ conversoes.js
@@ -19,4 +19,16 @@ const numeroString = "456";
 // converter o valor que numeroString que esta como string e não number
 //Number ()
 //String ()
-console.log (numero + Number (numeroString))
+//console.log (numero + Number (numeroString))
+
+//let telefone = 12341234;
+//console.log("O telefone é " + String(telefone)); // teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+
+let telefone = 12341234;
+console.log("O telefone é " + telefone.toString()); // o .toString() é uma outra forma para  fazer essa conversão, que é mais parecida com outras linguagens de programação.
+
+
+let usuarioConectado = false;
+console.log(String(usuarioConectado)); // teremos a conversão da booleana para string, nesse caso teremos uma string “false”.
+usuarioConectado = true;
+console.log(String(usuarioConectado)); // agora teremos uma string “true”.
