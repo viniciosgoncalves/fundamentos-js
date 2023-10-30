8f63be6467eada14c2bb8b90e213ad3936415420
Vinicios Jose Gonçalves de Jesus <vinicios.goncalves@compasso.com.br>
2023-10-30 16:07:21 -0300

    exmeplos da aula 3 console.log e console.error

---
+++ console.js
@@ -0,0 +1,10 @@
+//log -> registro
+const minhaVar = true;
+
+console.log (245)
+console.log ("eu sou um texto string")
+console.log (minhaVar)
+
+//tratamento de erros
+
+console.error("eu sou um erro!")
