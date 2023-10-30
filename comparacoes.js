aaccc398b8316400f88b28c61ed25a7ef0f4762d
Vinicios Jose Gonçalves de Jesus <vinicios.goncalves@compasso.com.br>
2023-10-30 16:33:01 -0300

    exemplos aula 4 operadores de comparação

---
+++ comparacoes.js
@@ -0,0 +1,16 @@
+// == comparação implícita
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto)
+
+// === comparação explícita
+const numero1 = 5;
+const texto1 = "5";
+
+console.log(numero === texto1)
+
+//typeof descobrir o tipo de dado que esta sendo salvo na variavel
+console.log(typeof numero)
+console.log(typeof texto)
